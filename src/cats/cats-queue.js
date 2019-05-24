2b8b2bb6ec6441adb64996f4243ee7b8b483b8d2
Joel Corey <joelcorey@fastmail.com>
2019-05-24 12:22:38 -0700

    fix get by id

--- src/cats/cats-queue.js
+++ src/cats/cats-queue.js
@@ -18,15 +18,21 @@ function getById(queue, id) {
   // console.log(`id: ${id}`);
   // console.log(`current.data.id: ${current.data.id} `);
 
-  if (current.data.id == id) {
-    return current.data;
-  }
-  console.log(current)
-  if(current.next === null || current.next === undefined) {
-    return { Error: 'Not found' }
+  while(current.data.id !== id) {
+
+    if(current.next === null || current.next === undefined) {
+      return { Error: 'Not found' }
+    }
+
+    current = current.next;
   }
 
-  getById(current.next, id)
+  if (current.data.id === id) {
+    console.log('found it')
+    return current.data;
+  }
+
+  //getById(current.next, id)
 }
 
 module.exports = { setInterval, cats, getById }
